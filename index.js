69b34a710df745da024fbbd8c6f5b624f26c9d95
Samuel Oluwatobi Oluwasegun <107188941+Oluwaseg@users.noreply.github.com>
2024-07-30 16:14:26 +0100

    update

--- index.js
+++ index.js
@@ -10,6 +10,30 @@ const headers = {
   Accept: "application/vnd.github.v3+json",
 };
 
+// axios
+//   .get(getReposUrl, { headers })
+//   .then((response) => {
+//     const repos = response.data;
+//     repos.forEach((repo) => {
+//       if (!repo.private) {
+//         const updateUrl = `https://api.github.com/repos/${GITHUB_USERNAME}/${repo.name}`;
+//         axios
+//           .patch(updateUrl, { private: true }, { headers })
+//           .then(() => {
+//             console.log(`Successfully made ${repo.name} private.`);
+//           })
+//           .catch((error) => {
+//             console.error(
+//               `Failed to make ${repo.name} private: ${error.response.status}`
+//             );
+//           });
+//       }
+//     });
+//   })
+//   .catch((error) => {
+//     console.error(`Failed to retrieve repositories: ${error.response.status}`);
+//   });
+
 axios
   .get(getReposUrl, { headers })
   .then((response) => {
@@ -18,13 +42,13 @@ axios
       if (repo.private) {
         const updateUrl = `https://api.github.com/repos/${GITHUB_USERNAME}/${repo.name}`;
         axios
-          .patch(updateUrl, { private: true }, { headers })
+          .patch(updateUrl, { private: false }, { headers })
           .then(() => {
-            console.log(`Successfully made ${repo.name} private.`);
+            console.log(`Successfully made ${repo.name} public.`);
           })
           .catch((error) => {
             console.error(
-              `Failed to make ${repo.name} private: ${error.response.status}`
+              `Failed to make ${repo.name} public: ${error.response.status}`
             );
           });
       }
